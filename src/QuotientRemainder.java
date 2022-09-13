21d0fc1f2da8c7dbcfe382f9f3ee91019dd13117
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-09-13 20:04:57 +0530

    Added: Program for Quotient & Remainder.

---
+++ src/QuotientRemainder.java
@@ -0,0 +1,12 @@
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int dividend = 38;
+        int divisor = 4;
+
+        int quotient = dividend/divisor;
+        int remainder = dividend%divisor;
+
+        System.out.println("Quotient is: "+quotient);
+        System.out.println("Remainder is: "+remainder);
+    }
+}
