f9c2ac25d43b23c607eb2de9d1f0a6186e0e3b13
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-09-14 11:09:02 +0530

    Added: Program for finding Prime factorization.

---
+++ src/Factors.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+
+        for (int i = 2; i <num; i++){
+            while(num%i==0){
+                System.out.println(i);
+                num = num/i;
+            }
+        }
+        if (num>2)
+            System.out.println(num);
+    }
+}
